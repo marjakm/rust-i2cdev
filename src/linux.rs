daf8f9f0bc919c1ed5bda6d067f936813062d8bd
Mattis Marjak <mattis.marjak@defendec.com>
2016-02-02 18:06:39 +0200

    make LinuxI2CDevice set_slave_address public

--- src/linux.rs
+++ src/linux.rs
@@ -94,7 +94,7 @@ impl LinuxI2CDevice {
     /// (it is done internally).  Calling this method is only
     /// necessary if you need to change the slave device and you do
     /// not want to create a new device.
-    fn set_slave_address(&mut self, slave_address: u16) -> I2CResult<()> {
+    pub fn set_slave_address(&mut self, slave_address: u16) -> I2CResult<()> {
         try!(ffi::i2c_set_slave_address(self.as_raw_fd(), slave_address));
         self.slave_address = slave_address;
         Ok(())
